b72955e73ee9d0c79c369330fda5c922da9f1af5
user <user@FC10>
2016-12-16 09:34:38 +0800

    add six class

---
+++ src/Pokemon.java
@@ -0,0 +1,30 @@
+
+public class Pokemon {
+
+	private final String name ;
+	private final PokemonType type;
+	private int cp;
+	public String getName() {
+		return name;
+	}
+
+	public PokemonType getType() {
+		return type;
+	}
+	
+	public int getCp() {
+		return cp;
+	}
+	public void setCp(int cp) {
+		this.cp = cp;
+	}
+	public Pokemon(String name, PokemonType type, int cp) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.cp = cp;
+	}
+
+	
+
+}
